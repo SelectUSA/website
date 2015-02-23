87b288e38edb45a60b37638cd9330b18552c6bb0
AJ Singh <ajsingh273@gmail.com>
2015-02-23 09:08:59 -0500

    New Federal Incentives first commit

---
+++ articles.js
@@ -0,0 +1,82 @@
+var movies = [
+  {
+    "name": "Clean Energy Loan Guarantee Program",
+    "link": "http://selectusa.github.io/website/clean-energy-loan-guarantee-program.html",
+    "outline": "The Department of Energy’s Loan Programs—administered by LPO—enable DOE to work with private companies and lenders to mitigate the financing risks associated with clean energy projects, and thereby encourage their development on a broader and much-needed scale. The Loan Programs consist of three separate programs managed by two offices, the Loan Guarantee Program Office (LGP) and the Advanced Technology Vehicles Manufacturing Loan Program Office. LPO originates, guarantees, and monitors loans to support clean energy projects through these programs. The programs are:",
+    "rating": 9.2,
+    "director": "Francis Ford Coppola",
+    "year": 1972,
+    "stars": [
+      "Marlon Brando",
+      "Al Pacino",
+      "James Caan"
+    ],
+    "runtime": 175,
+    "genre": [
+      "Energy",
+      "Environment",
+      "Environment and Energy",
+      "Funding",
+      "Petroleum, Minerals and Mining",
+      "Research and Development",
+      "Department of Energy"
+    ],
+    "certificate": "R",
+    "date": "1974-07-12T10:29:29+05:30",
+    "actor": "Marlon Brando",
+    "id": 1
+  },
+  {
+    "name": "Grants.gov",
+    "link": "http://selectusa.github.io/website/grantsgov.html",
+    "outline": "Grants.gov is your source to find and apply for federal grants. Grants.gov is a central storehouse for information on grants from multiple federal agencies. Grants.gov lists over 1,000 grant programs and provides access to approximately $500 billion in annual awards. Government organization, educational organization, public housing organization, non-profit organizations, small businesses, other for-profit organizations, individuals are all eligible to apply for different federal governemnt grants.",
+    "rating": 9.2,
+    "director": "Francis Ford Coppola",
+    "year": 1972,
+    "stars": [
+      "Marlon Brando",
+      "Al Pacino",
+      "James Caan"
+    ],
+    "runtime": 175,
+    "genre": [
+      "Aerospace",
+      "Agribusiness and Forest Products",
+      "Automotive",
+      "Chemicals and Pharmaceuticals",
+      "Construction and Real Estate",
+      "Consumer Goods",
+      "Creative and Media",
+      "Defense and Security",
+      "Education",
+      "Education and Training",
+      "Electronics and Industrial Equipment",
+      "Energy",
+      "Environment",
+      "Environment and Energy",
+      "Financial Services",
+      "Food, Beverage and Restaurant",
+      "Funding",
+      "Health",
+      "Information and Communication Technology",
+      "Life Sciences and Medical",
+      "Logistics and Transportation",
+      "Manufacturing",
+      "Petroleum, Minerals and Mining",
+      "Physical Science and Engineering",
+      "Professional Services (Legal, Accounting, etc.)",
+      "Real Estate",
+      "Research and Development",
+      "Retail Trade",
+      "Textiles",
+      "Travel, Tourism and Hospitality",
+      "U.S. Companies",
+      "Wholesale and Retail Trade",
+      "Information"
+    ],
+    "certificate": "R",
+    "date": "1974-07-12T10:29:29+05:30",
+    "actor": "Marlon Brando",
+    "id": 2
+  }
+]
